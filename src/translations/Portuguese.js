c45a543a4dcb4b5c6e2f55e95f4eea7e2668a1ee
bitafro <grupol4pt@gmail.com>
2017-10-05 21:53:35 +0100

    Create Portuguese.js
    Hi made translation for the Portuguese Language
    Any more translations please ask
    grupol4pt@sapo.pt
    charlie777pt
    https://steemit.com/@charlie777pt

---
+++ src/translations/Portuguese.js
@@ -0,0 +1,86 @@
+export const PORTUGUESE_TRANSLATION = {
+General: {
+loading: 'A carregar...',
+address: 'Endereço',
+privateKey: 'Chave Privada',
+cancel: 'Cancelar', 
+fees: 'Taxas',
+version: 'Versão',
+in: 'Entrada',
+out: 'Saída'
+},
+MainPage: {
+title: 'Carteira ZEN',
+value: 'Valor', 
+send: 'Enviar',
+received: 'Recebido',
+sent: 'Enviado', 
+noTxFound: 'Não foi encontrada história de transações.'
+},
+AddressInfoPage: {
+copyToClipboard: 'Copiar Endereço para o Clipboard',
+},
+SendPage: {
+title: 'Enviar ZEN',
+payTo: 'Pagar a',
+amountToPay: 'Quantia a Pagar',
+balance: 'Balanço',
+networkFee: 'Taxa da Rede',
+slowTx: 'Tx Lento',
+fastTx: 'Tx Rápido', 
+from: 'De',
+toAddress: 'Para Endereço',
+amount: 'Quantia', 
+max: 'Max', 
+send: 'Enviar',
+txSuccessful: 'Transaçao com Sucesso! Clique aqui para ver a sua transação.',
+confirmSend: 'Quero enviar estes ZEN',
+invalidAddress: 'Inválido `Para Endereço` Só Endereços transparentes são supotados por enquanto.',
+invalidAmount: 'Inválido `Quantia`.',
+invalidFee: 'Inválido `Taxas`. Tente 0 :)',
+zeroAmount: 'Quantia tem de ser maior do que 0.',
+notEnoughZEN: 'Não tem suficientes ZEN confirmados na conta para efetuar a transação.',
+noCameraPermissions: 'Camera não permitida. Pode autorizar camera nas suas configurações.'
+},
+TxDetailPage:{
+txid: 'Id da Transção',
+blockhash: 'Block Hash',
+blockheight: 'Block Height',
+confirmations: 'Confirmações', 
+},
+SettingsPage: {
+title: 'Configurações', 
+language: 'Língua',
+currency: 'Moeda',
+secretPhrase: 'Mostrar Frase Secreta',
+showPrivateKeys: 'Mostrar Chaves Privadas',
+recoverExistingWallet: 'Recuperar carteira existente',
+current: 'Atual'
+}, 
+SecretPhrasePage: {
+title: 'Frase Secreta'
+},
+ShowPrivateKeyPage: {
+title: 'Chaves Privadas'
+},
+RecoverExistingWalletPage: {
+title: 'Recover Existing Wallet',
+secretPhrase: 'Frase Secreta',
+textareaPlaceholder: 'Frase Secreta. min 16 caracteres',
+confirmUnderstand: 'Eu compreendo que recuperando a carteira existente, a minha carteira atual será apagada.',
+recover: 'Recuperar'
+},
+AboutPage: {
+title: 'Sobre',
+},
+PinPage: {
+changePinTitle: 'Mudar PIN',
+newPinPageTitle: 'Configuração carteira ZEN',
+verifyPinPageTitle: 'Carteira ZEN PIN Verificação',
+setupNewPin: 'Configure um novo PIN',
+reenterPin: 'Re-insira o seu PIN',
+pinsNotSimilar: 'PINs inserido não corresponde',
+invalidPin: 'PIN Inválido',
+enterYourPin: 'Insira o seu PIN'
+}
+}
